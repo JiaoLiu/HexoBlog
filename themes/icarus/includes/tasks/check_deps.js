8dcd7d71d850ab9ee5bad23c2b3ebe54b166a25c
刘蛟 <jiao1.liu@changhong.com>
2019-01-10 10:22:16 +0800

    change markdown parser

--- themes/icarus/includes/tasks/check_deps.js
+++ themes/icarus/includes/tasks/check_deps.js
@@ -1,32 +1,37 @@
-const logger = require('hexo-log')();
+const logger = require('hexo-log')()
 
 function checkDependency(name) {
-    try {
-        require.resolve(name);
-        return true;
-    } catch(e) {
-        logger.error(`Package ${name} is not installed.`)
-    }
-    return false;
+  try {
+    require.resolve(name)
+    return true
+  } catch (e) {
+    logger.error(`Package ${name} is not installed.`)
+  }
+  return false
 }
 
-logger.info('Checking dependencies');
+logger.info('Checking dependencies')
 const missingDeps = [
-    'js-yaml',
-    'moment',
-    'cheerio',
-    'hexo-util',
-    'hexo-log',
-    'hexo-pagination',
-    'hexo-generator-archive',
-    'hexo-generator-category',
-    'hexo-generator-index',
-    'hexo-generator-tag',
-    'hexo-renderer-ejs',
-    'hexo-renderer-marked',
-    'hexo-renderer-stylus',
-].map(checkDependency).some(installed => !installed);
+  'js-yaml',
+  'moment',
+  'cheerio',
+  'hexo-util',
+  'hexo-log',
+  'hexo-pagination',
+  'hexo-generator-archive',
+  'hexo-generator-category',
+  'hexo-generator-index',
+  'hexo-generator-tag',
+  'hexo-renderer-ejs',
+  // 'hexo-renderer-marked',
+  'hexo-renderer-markdown-it',
+  'hexo-renderer-stylus'
+]
+  .map(checkDependency)
+  .some(installed => !installed)
 if (missingDeps) {
-    logger.error('Please install the missing dependencies from the root directory of your Hexo site.');
-    process.exit(-1);
+  logger.error(
+    'Please install the missing dependencies from the root directory of your Hexo site.'
+  )
+  process.exit(-1)
 }
